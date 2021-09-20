0c1c52da5a1274a201d8759c991e937b5d244f78
nocturnalcoders <nocturnalcoders@github.com>
2021-09-20 13:42:20 +0700

    git push

---
+++ src/config/API/index.js
@@ -0,0 +1,9 @@
+export const API_HOST = {
+    url: 'http://139.180.144.215:8080/api/v1',
+  };
+  
+  
+  export const API_HOST_ARTICLE = {
+    url: 'http://backend-ekost.herokuapp.com/api/v1',
+  };
+  
